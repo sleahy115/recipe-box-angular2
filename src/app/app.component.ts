156bcbef5254d71c06f703e44949d9e2a468b397
Sarah Leahy <sleahy115@gmail.com>
2017-03-28 11:15:02 -0700

    create initial files with recipe class

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Recipe-box';
+  recipes: Recipe[] = [
+    new Recipe('Pizza','crust,sauce,pepperoni','bake and enjoy'),
+    new Recipe('Tacos','tortillas,meat, seasoning','brown meat, add seasoning, put int totilla'),
+    new Recipe('Pasta','tomatoes, shrimp, seasoning','cook tomoatoes, add chicken, stir into pasta')
+  ];
+}
+
+export class Recipe {
+  constructor(public title: string, public ingredients: string, public directions: string) { }
+}
